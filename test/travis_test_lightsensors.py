5fc88c7468d8486dde7e84a944580b36123948ce
k-nakamura <k-nakamura@figla.co.jp>
2017-11-27 19:29:22 +0900

    7th test

--- test/travis_test_lightsensors.py
+++ test/travis_test_lightsensors.py
@@ -37,7 +37,7 @@ class LightsensorTest(unittest.TestCase):
     time.sleep(3)
 
     self.assertFalse(self.count == 0, "cannot subscribe the topic")
-    self.check_values(-1, 0, 123, 4321)
+    self.check_values(4321,123,0,-1)
 
   def test_change_parameter(self):
     rospy.set_param('lightsensors_freq', 1)
