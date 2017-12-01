ec3411dfd554378a706632afcb0dd6d942bc80aa
k-nakamura <k-nakamura@figla.co.jp>
2017-12-01 14:37:01 +0900

    test

--- test/travis_test_motors_back.py
+++ test/travis_test_motors_back.py
@@ -14,7 +14,7 @@ class MotorTest(unittest.TestCase):
   def test_node_exist(self):
     nodes = rosnode.get_node_names()
     self.assertIn('/motors',nodes,"node does not exist")
- 
+
   def test_put_freq(self):
     pub = rospy.Publisher('/motor_raw', MotorFreqs)
     m = MotorFreqs()
@@ -23,6 +23,7 @@ class MotorTest(unittest.TestCase):
     for i in range(10):
       pub.publish(m)
       time.sleep(0.1)
+
     self.file_check("rtmotor_raw_l0",m.left_hz,"wrong left value from motor_raw")
     self.file_check("rtmotor_raw_r0",m.right_hz,"wrong right value from motor_raw")
 
@@ -35,15 +36,14 @@ class MotorTest(unittest.TestCase):
       pub.publish(m)
       time.sleep(0.1)##
 
-    self.file_check("rtmotor_raw_r0",200,"wrong right value from cmd_vel")
-    self.file_check("rtmotor_raw_l0",600,"wrong left value from cmd_vel")
+    self.file_check("rtmotor_raw_l0",200,"wrong right value from cmd_vel")
+    self.file_check("rtmotor_raw_r0",600,"wrong left value from cmd_vel")
   
     time.sleep(1.1)
-    self.file_check("rtmotor_raw_l0",0,"don't stop after 1[s]")
     self.file_check("rtmotor_raw_r0",0,"don't stop after 1[s]")
+    self.file_check("rtmotor_raw_l0",0,"don't stop after 1[s]")
 
 if __name__=='__main__':
-  time.sleep(3)
   rospy.init_node('travis_test_motors')
   rostest.rosrun('pimouse_ros','travis_test_motors',MotorTest)
 
