ea524a6a9ec263e5329d46f05ec69aa2a156c91a
k-nakamura <k-nakamura@figla.co.jp>
2017-12-05 17:22:18 +0900

    actionlib

--- test/travis_test_buzzer.py
+++ test/travis_test_buzzer.py
@@ -33,7 +33,7 @@ class BuzzerTest(unittest.TestCase):
 
     self.client.wait_for_server()
     self.client.send_goal(goal,feedback_cb=self.feedback_cb)
-    self.client.wait_result()
+    self.client.wait_for_result()
 
     self.assertTrue(self.client.get_result(),"invalid result")
     self.assertEqual(goal.freqs,self.device_values,"invalid feedback:"
